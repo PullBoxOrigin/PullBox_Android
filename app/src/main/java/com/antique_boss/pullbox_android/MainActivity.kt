7be8bade95e6626c3d8cfa718305cb89908c840e
kim jeongg hoon <48915641+chkpizza@users.noreply.github.com>
2023-01-07 09:20:35 +0900

    make android project

---
+++ app/src/main/java/com/antique_boss/pullbox_android/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.antique_boss.pullbox_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
