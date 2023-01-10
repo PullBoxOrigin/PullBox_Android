412cb0161d70630d68e8df7e1d9d13aee4fefb4a
kim jeongg hoon <48915641+chkpizza@users.noreply.github.com>
2023-01-10 14:04:12 +0900

    [FEATURE-registration] setup toolbar
    1. add toolbar
    2. setup toolbar item default value to false
    3. setup toolbar item default click listener

--- features/registration/src/main/java/com/antique_boss/registration/RegistrationFragment.kt
+++ features/registration/src/main/java/com/antique_boss/registration/RegistrationFragment.kt
@@ -1,10 +1,10 @@
 package com.antique_boss.registration
 
 import android.os.Bundle
+import android.view.*
+import android.widget.Toast
+import androidx.core.view.MenuHost
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import com.antique_boss.registration.databinding.FragmentRegistrationBinding
 
@@ -12,11 +12,39 @@ class RegistrationFragment : Fragment() {
     private var _binding: FragmentRegistrationBinding? = null
     private val binding get() = _binding!!
 
+    private lateinit var completeMenuItem: MenuItem
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = DataBindingUtil.inflate(inflater, R.layout.fragment_registration, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initialize()
+    }
+
+    private fun initialize() {
+        setupToolbar()
+
+    }
+
+    private fun setupToolbar() {
+        binding.registrationToolbar.inflateMenu(R.menu.registration_toolbar_menu)
+        completeMenuItem = binding.registrationToolbar.menu.findItem(R.id.registration)
+        completeMenuItem.isEnabled = false
+
+        binding.registrationToolbar.setOnMenuItemClickListener {
+            when(it.itemId) {
+                R.id.registration -> {
+                    Toast.makeText(requireActivity(), "Clicked", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
