8e9d11b751c727dfb1143385aefcc5d9d3747a38
kim jeongg hoon <48915641+chkpizza@users.noreply.github.com>
2023-01-14 13:40:56 +0900

    [FEATURE-portfolio] dynamic add comment

--- features/portfolio/src/main/java/com/antique_boss/portfolio/viewmodel/PortfolioViewModel.kt
+++ features/portfolio/src/main/java/com/antique_boss/portfolio/viewmodel/PortfolioViewModel.kt
@@ -16,11 +16,17 @@ class PortfolioViewModel : ViewModel() {
     private val _comments = MutableLiveData<MutableList<String>>()
     val comments: LiveData<MutableList<String>> get() = _comments
 
+    private val _submit = MutableLiveData<Boolean>()
+    val submit: LiveData<Boolean> get() = _submit
+
     fun fetchPreview(url: String) {
         viewModelScope.launch {
             _preview.value = PreviewCrawler().getPreview(url)
         }
     }
+
+    private val _comment = MutableLiveData<String>()
+    val comment: LiveData<String> get() = _comment
 
     fun loadComments() {
         if(_comments.value.isNullOrEmpty()) {
@@ -48,6 +54,20 @@ class PortfolioViewModel : ViewModel() {
                     "20 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five "
                 )
             }
+        }
+    }
+
+    fun updateComment(comment: String) {
+        _comment.value = comment
+    }
+
+    fun validateRequiredElement() {
+        _submit.value = _comment.value?.isNotEmpty() == true
+    }
+
+    fun attachComment() {
+        _comment.value?.let {
+            _comments.value?.add(0, it)
         }
     }
 }
